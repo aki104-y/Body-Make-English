d50e9df0e688fcd0c38b8e74a3ca5c927227a835
akitoshi-y <aomine6@gmail.com>
2022-06-25 22:30:22 +0900

    css,、js追加

--- common/js/script.js
+++ common/js/script.js
@@ -1,22 +1,40 @@
-$(function(){
+$(function () {
+
+  //フェードイン(ふわっと)表示
+  //--------------------------------------------
+  $(window).scroll(function () {
+    $(".fadein").each(function () {
+      // 要素の位置を取得 
+      var elemPos = $(this).offset().top;
+      // スクロール位置を取得
+      var scroll = $(window).scrollTop();
+      // 画面幅を取得（画面の下側に入ったときに動作させるため）
+      var windowHeight = $(window).height(); 
+      if (scroll > elemPos - windowHeight + 100) {
+        // 要素位置までスクロール出来たときに動作する
+        $(this).addClass("fadein-scroll");
+      }
+    });
+  });
+  jQuery(window).scroll();
 
   //トレーナーのタブエリア
   //--------------------------------------------
-    //スタッフエリアを非表示
-    $('.grid--staff').hide();
-    //タブをクリック
-    $(".tab-navigation__link").click(function(){
+  //スタッフエリアを非表示
+  $('.grid--staff').hide();
+  //タブをクリック
+  $(".tab-navigation__link").click(function () {
     //一度全てのコンテンツを非表示
-      $(".grid").hide();
+    $(".grid").hide();
     //選択されたコンテンツを再表示  
-      $($(this).attr("href")).show();
+    $($(this).attr("href")).show();
     //現在の付与しているクラスを削除
-      $(".tab-navigation__link--active").removeClass("tab-navigation__link--active");
+    $(".tab-navigation__link--active").removeClass("tab-navigation__link--active");
     //選択されたタブにクラスを追加
-      $(this).addClass("tab-navigation__link--active");
-  
-      return false;
-    })
+    $(this).addClass("tab-navigation__link--active");
+
+    return false;
+  })
 
 
   //swiper
